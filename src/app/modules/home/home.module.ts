8cbf4f35439aaf4caa9b0102f0bebbc0fa56c46f
Pritam <debnathpritam0802@gmail.com>
2024-09-21 12:17:42 +0530

    ng g c  modules/home/components/home

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,

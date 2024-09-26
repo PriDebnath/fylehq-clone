249fa5680018193e5151969eb74acd0990cf4f15
Pritam <debnathpritam0802@gmail.com>
2024-09-26 10:33:02 +0530

    Done upto 'Tired of chasing employees...'

---
+++ src/app/modules/home/components/headline-and-subline/headline-and-subline.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-headline-and-subline',
+  templateUrl: './headline-and-subline.component.html',
+  styleUrl: './headline-and-subline.component.css',
+})
+export class HeadlineAndSublineComponent {
+  @Input() containerClass?: { [key: string]: boolean };
+  @Input() containerStyle?: { [key: string]: string };
+  @Input() headline: string = 'headline';
+  @Input() subline: string = 'subline';
+}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -6,9 +6,10 @@ import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { NgZorroAntdModule } from '../ng-zorro-antd/ng-zorro-antd.module';
 import { HeaderComponent } from './components/header/header.component';
+import { HeadlineAndSublineComponent } from './components/headline-and-subline/headline-and-subline.component';
 
 @NgModule({
-  declarations: [NavComponent, HomeComponent, HeaderComponent],
+  declarations: [NavComponent, HomeComponent, HeaderComponent, HeadlineAndSublineComponent],
   imports: [CommonModule, NgZorroAntdModule, HomeRoutingModule],
 })
 export class HomeModule {}

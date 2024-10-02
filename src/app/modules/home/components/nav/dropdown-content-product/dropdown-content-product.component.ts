698bf8178a810f126b2977702cb9001203787f06
Pritam <debnathpritam0802@gmail.com>
2024-10-02 15:55:17 +0530

    Component to display product drop down

---
+++ src/app/modules/home/components/nav/dropdown-content-product/dropdown-content-product.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { svg } from '../svg';
+
+@Component({
+  selector: 'app-dropdown-content-product',
+  templateUrl: './dropdown-content-product.component.html',
+  styleUrl: './dropdown-content-product.component.css'
+})
+export class DropdownContentProductComponent {
+  svg = svg;
+
+}

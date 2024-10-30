986ec7437d9c3b71ccc77ec5a002a2fd5f7cd3cc
Pritam <debnathpritam0802@gmail.com>
2024-10-30 22:25:07 +0530

    css + iamge changes

--- src/app/modules/ng-zorro-antd/ng-zorro-antd.module.ts
+++ src/app/modules/ng-zorro-antd/ng-zorro-antd.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, NzButtonModule, NzDropDownModule],
-  exports: [NzButtonModule, NzDropDownModule],
+  imports: [
+    CommonModule,
+    NzModalModule,
+    NzButtonModule,
+    NzSelectModule,
+    NzDropDownModule,
+    NzCheckboxModule,
+  ],
+  exports: [
+    NzButtonModule,
+    NzSelectModule,
+    NzModalModule,
+    NzDropDownModule,
+    NzCheckboxModule,
+  ],
 })
 export class NgZorroAntdModule {}
